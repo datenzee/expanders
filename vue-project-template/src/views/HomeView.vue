0e75ac045f928a7f75a417f3b6380ee0e8d9b671
Jan Slifka <jan.slifka@gmail.com>
2022-06-21 08:46:45 +0200

    Update root container name

--- vue-project-template/src/views/HomeView.vue
+++ vue-project-template/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import * as $rdf from 'rdflib'
 import data from './data'
-import DMPContainer from '@/components/gen/DMPContainer.vue'
+import DMPContainer from '@/components/gen/Container_Root.vue'
 
 const object = $rdf.sym('https://demo.ds-wizard.org/questionnaires/00f2e7b7-a1c8-42a1-a2fc-2f93dbdbaa76')
 const graph = new $rdf.IndexedFormula()
