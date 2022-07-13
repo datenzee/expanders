c1726715bf4906f747fd0c2109407ede705e2ac8
Jan Slifka <jan.slifka@gmail.com>
2022-07-13 15:56:35 +0200

    Add data loading for the generated app

--- README.md
+++ README.md
@@ -21,3 +21,13 @@ $ expander --dev <path-to-owl> <path-to-out-dir>
 ```
 
 By default the application is running on http://localhost:8081/.
+
+You can use GET parameters to specify data source and root node in the graph with:
+
+- `data` - full URL of the data
+- `root` - full IRI of the root node in the graph
+
+**Example**
+```
+http://localhost:8081/?data=https://api.datenzee.ds-wizard.org/questionnaires/4dac4509-f0a3-4346-b7ce-586a3098b100/documents/preview&root=https://demo.ds-wizard.org/questionnaires/00f2e7b7-a1c8-42a1-a2fc-2f93dbdbaa76
+```
--- vue-project-template/src/App.vue
+++ vue-project-template/src/App.vue
@@ -2,13 +2,48 @@
 import * as $rdf from 'rdflib'
 import data from './data'
 import RootContainer from '@/components/gen/Container_Root.vue'
+import {reactive} from "@vue/reactivity";
+import axios from "axios";
 
-const object = $rdf.sym('https://demo.ds-wizard.org/questionnaires/00f2e7b7-a1c8-42a1-a2fc-2f93dbdbaa76')
-const graph = new $rdf.IndexedFormula()
-$rdf.parse(data, graph, object.uri, 'text/turtle')
+const state = reactive({
+    object: null,
+    graph: null,
+    loading: true,
+    error: null,
+})
+
+const url = new URL(window.location.href)
+
+const root = url.searchParams.get('root') || 'https://demo.ds-wizard.org/questionnaires/00f2e7b7-a1c8-42a1-a2fc-2f93dbdbaa76'
+state.object = $rdf.sym(root)
+
+const createGraph = (turtle) => {
+    try {
+      const graph = new $rdf.IndexedFormula()
+      $rdf.parse(turtle, graph, state.object.uri, 'text/turtle')
+      state.graph = graph
+      state.loading = false
+    } catch (error) {
+      state.loading = false
+      state.error = error.toString()
+    }
+}
+
+const dataUrl = url.searchParams.get('data')
+if (dataUrl) {
+    axios
+        .get(dataUrl)
+        .then((result) => {
+            createGraph(result.data)
+        })
+} else {
+    createGraph(data)
+}
 </script>
 <template>
-  <main>
-    <RootContainer :graph="graph" :depth="0" :object="object" />
-  </main>
+    <main>
+        <div v-if="state.loading">Loading...</div>
+        <div v-else-if="state.error" class="text-danger">{{ state.error }}</div>
+        <RootContainer v-else :graph="state.graph" :depth="0" :object="object"/>
+    </main>
 </template>
