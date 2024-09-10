27865377b5f22f9cf7ca8b0c7826e3d3a7af354e
Vojtech Knaisl <vknaisl@gmail.com>
2024-09-10 11:03:26 +0200

    Add harvesting for doc

--- expander/cli.py
+++ expander/cli.py
@@ -3,6 +3,7 @@ import sys
 import click
 
 from expander.doc.expander import DocExpander
+from expander.doc.harvester import harvest
 from expander.vue.expander import VueExpander
 
 
@@ -11,9 +12,10 @@ from expander.vue.expander import VueExpander
 @click.argument('root_component')
 @click.argument('input_file')
 @click.argument('output_dir')
+@click.option('-h', '--harvest-dir', required=False)
 @click.option('-d', '--dev', is_flag=True)
 @click.option('-e', '--expand-only', is_flag=True)
-def main(type, root_component, input_file, output_dir, dev, expand_only):
+def main(type, root_component, input_file, output_dir, harvest_dir, dev, expand_only):
     print(f'Expanding {input_file} into {output_dir}')
     if type == 'vue':
         expander = VueExpander(root_component, output_dir, input_source=input_file)
@@ -21,7 +23,11 @@ def main(type, root_component, input_file, output_dir, dev, expand_only):
         if not expand_only:
             expander.post_expand(dev)
     elif type == 'doc':
-        expander = DocExpander(root_component, output_dir, input_source=input_file)
+        if harvest_dir is not None:
+            harvested_data = harvest(harvest_dir)
+        else:
+            harvested_data = {}
+        expander = DocExpander(root_component, output_dir, harvested_data, input_source=input_file)
         expander.expand()
         if not expand_only:
             expander.post_expand(dev)
---
+++ expander/doc/harvester.py
@@ -0,0 +1,76 @@
+import os
+import shutil
+import subprocess
+from glob import glob
+from pprint import pprint
+
+
+def download_harvested_files(tmp_harvested_dir):
+    template_dir = os.path.join(os.path.dirname(__file__), '..', '..', 'project-templates', 'doc', 'document_template')
+    shutil.copytree(template_dir, tmp_harvested_dir, dirs_exist_ok=True)
+    subprocess.check_call('dsw-tdk get datenzee:horizon-europe-expanded-template:1.0.0', shell=True,
+                          cwd=tmp_harvested_dir)
+
+
+def harvest(harvest_dir):
+    harvested_data = {}
+    files = get_files(harvest_dir)
+    parts_to_remove = [
+        harvest_dir,
+        '/src/components/gen/', '.html.jinja2',
+        '/datenzee_horizon-europe-expanded-template_1.0.0'
+    ]
+    for file in files:
+        component = file
+        for part_to_remove in parts_to_remove:
+            component = component.replace(part_to_remove, '')
+        before = extract_content(file, "before")
+        if is_not_empty(before):
+            harvested_data[component + "_before"] = before
+        after = extract_content(file, "after")
+        if is_not_empty(after):
+            harvested_data[component + "_after"] = after
+    print(" *** Harvested data (start) ***")
+    pprint(harvested_data)
+    print(" *** Harvested data (end) ***")
+    return harvested_data
+
+
+def is_not_empty(value):
+    return value is not None and value != 'None' and value != [] and value != [''] and value != ''
+
+
+def get_files(harvest_dir):
+    skip_files = [
+        'Heading.html.jinja2',
+        'IterativeContainer.html.jinja2',
+        'Rdf.html.jinja2',
+    ]
+
+    files = []
+    pattern = "*.html.jinja2"
+    for dir, _, _ in os.walk(harvest_dir):
+        files.extend(glob(os.path.join(dir, pattern)))
+
+    return list(filter(lambda f: f not in skip_files, files))
+
+
+def extract_content(filename, stage):
+    with open(filename, 'r') as file:
+        data = file.read()
+
+    # Define the start and end markers
+    start_marker = "{#  Customization (" + stage + "): start  #}"
+    end_marker = "{#  Customization (" + stage + "): end  #}"
+
+    # Find all content between the markers
+    start = 0
+    while True:
+        start = data.find(start_marker, start)
+        if start == -1:
+            break
+        start += len(start_marker)
+        end = data.find(end_marker, start)
+        if end == -1:
+            break
+        return data[start:end].strip()
